2d96fe0233f937148b385b7e85c290f9723d0c1d
Mirela Manoleva <mirelam99@gmail.com>
2024-03-26 23:23:49 +0200

    Parse game info to object, separate JSON parsing functionality

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ func main() {
 		println("Couldn't load previous records - " + err.Error())
 	}
 
-	defer func () {
-			err = limiter.SaveRequestsMade()
+	defer func() {
+		err = limiter.SaveRequestsMade()
 		// Should also add option for retry
 		if err != nil {
 			println("Couldn't save records. Please wait for 120 seconds before starting the program again - " + err.Error())
 		}
-	} ()
+	}()
 
 	PUUID, err := GetPUUID("Alerim", "EUNE")
 	if err != nil {
